11a9d9c866ea4922b1da2be2d62d2dffde6577f8
Gilson Gilbert <gilzon.me@gmail.com>
2025-02-23 16:24:16 +0530

    Deploy

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var react_1 = require("react");
-var Button = function (_a) {
-    var children = _a.children, onClick = _a.onClick, _b = _a.variant, variant = _b === void 0 ? "primary" : _b;
-    return ((0, jsx_runtime_1.jsx)("button", { onClick: onClick, className: "px-4 py-2 rounded ".concat(variant === "primary" ? "bg-blue-600 text-white" : "bg-gray-300 text-black"), children: children }));
-};
-exports.default = Button;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import clsx from "clsx";
 
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   variant?: "primary" | "secondary";
+  className?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, variant = "primary" }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, variant = "primary", className }) => {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 rounded ${
-        variant === "primary" ? "bg-blue-600 text-white" : "bg-gray-300 text-black"
-      }`}
+      className={clsx(
+        "px-4 py-2 rounded transition duration-200",
+        variant === "primary"
+          ? "bg-blue-600 text-white hover:bg-blue-700"
+          : "bg-gray-300 text-black hover:bg-gray-400",
+        className
+      )}
     >
       {children}
     </button>
